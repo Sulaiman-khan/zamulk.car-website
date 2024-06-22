2b981acafeab3b519cfb68cc456a4fd49f2c34ed
sefaizanahmad <sefaizanahmad@gmail.com>
2024-06-22 15:44:50 +0500

    add loader and logo and some animations all done

---
+++ assets/js/loader-main.js
@@ -0,0 +1,5 @@
+// Simulate delay for demonstration purposes
+setTimeout(function() {
+    // Add loaded class to body to show content and hide loader
+    document.body.classList.add('loaded');
+}, 2500); // Change delay time as needed (in milliseconds)
