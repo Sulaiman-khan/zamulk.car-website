b96d84d80c5772aaa6088cd4b573c6644521dca5
sefaizanahmad <sefaizanahmad@gmail.com>
2024-06-22 16:16:14 +0500

    loader setting

--- assets/js/loader-main.js
+++ assets/js/loader-main.js
@@ -1,5 +1,11 @@
-// Simulate delay for demonstration purposes
-setTimeout(function() {
-    // Add loaded class to body to show content and hide loader
-    document.body.classList.add('loaded');
-}, 2500); // Change delay time as needed (in milliseconds)
+document.addEventListener("DOMContentLoaded", function() {
+    // Simulate delay for demonstration purposes
+    setTimeout(function() {
+        // Add loaded class to body to show content and hide loader
+        document.body.classList.add('loaded');
+    }, 4500); // Change delay time as needed (in milliseconds)
+
+    // Trigger 360-degree rotation animation on loader image
+    const loaderImg = document.querySelector('.loader img');
+    loaderImg.style.animation = 'rotate 4s linear forwards'; // Adjust duration and timing function as needed
+});
