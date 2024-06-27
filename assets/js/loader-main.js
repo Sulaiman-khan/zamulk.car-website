8705b324cbefd258bbb1d905f688c28d5e1a39cc
sulaiman-khan <sallokhan01999@gmail.com>
2024-06-27 23:35:56 +0500

    change home page

--- assets/js/loader-main.js
+++ assets/js/loader-main.js
@@ -3,9 +3,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         // Add loaded class to body to show content and hide loader
         document.body.classList.add('loaded');
-    }, 4500); // Change delay time as needed (in milliseconds)
-
-    // Trigger 360-degree rotation animation on loader image
-    const loaderImg = document.querySelector('.loader img');
-    loaderImg.style.animation = 'rotate 4s linear forwards'; // Adjust duration and timing function as needed
+    }, 1); // Change delay time as needed (in milliseconds)
 });
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -15,7 +15,7 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 3500,
+    delay: 4500,
     disableOnInteraction: false,
   },
   pagination: {
